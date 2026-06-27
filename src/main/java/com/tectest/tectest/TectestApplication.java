88dec9a0db118a1073486364d5f32423901efe41
“IA-Cross” <ayanegui.isaac@gmail.com>
2026-06-27 16:14:10 -0600

    first commit

---
+++ src/main/java/com/tectest/tectest/TectestApplication.java
@@ -0,0 +1,13 @@
+package com.tectest.tectest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TectestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TectestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tectest/tectest/TectestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tectest.tectest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TectestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
